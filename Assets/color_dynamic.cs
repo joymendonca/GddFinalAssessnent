5bad6b1c0ed626c805899231766e06c454496f08
Bob The Builder <bob@the.builder>
2021-03-10 21:10:57 +0530

    First Commit

---
+++ Assets/color_dynamic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_dynamic : MonoBehaviour
+{
+    public Material[] RandomMaterials;
+    int RandomMat;
+    void Start()
+    {
+        RandomMat = Random.Range(0, 4);
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = RandomMaterials[RandomMat];
+
+    }
+}
